8df0ae3b15befa76d49eec533ccf275d5b2a7e10
Anders Christiansen Sørby <anders@sorby.xyz>
2021-09-23 16:58:05 +0200

    Add a nix flake setup

---
+++ flake.nix
@@ -0,0 +1,52 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-21.05";
+    lean = {
+      url = "github:leanprover/lean4";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    { self
+    , flake-utils
+    , nixpkgs
+    , lean
+    }:
+    flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = import nixpkgs { inherit system; };
+      packageName = "Lake";
+      src = ./.;
+      leanPkgs = lean.packages.${system};
+      inherit (leanPkgs) leanc stage1;
+      inherit (pkgs.lib) concatStrings debug;
+      project = leanPkgs.buildLeanPackage {
+        name = packageName;
+        inherit src;
+      };
+    in
+    {
+      packages.${packageName} = project.lean-package;
+
+      defaultPackage = self.packages.${system}.${packageName};
+
+      apps.lake = flake-utils.lib.mkApp {
+        name = "lake";
+        drv = project.executable;
+      };
+
+      defaultApp = self.apps.${system}.lake;
+
+      # `nix develop`
+      devShell = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          lean-pkgs.lean
+        ];
+      };
+    });
+}
